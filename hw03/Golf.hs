0b4dee1861d99994adaf2a156a325b9696d43527
Tony Poerio <tony@tonypoer.io>
2017-08-27 14:32:43 -0400

    Finished local maxima problem

--- hw03/Golf.hs
+++ hw03/Golf.hs
@@ -1,5 +1,4 @@
 module Golf where
-
 -- Hopscotch
 skips :: [a] -> [[a]]
 skips xs = skips' xs (length xs)
@@ -13,3 +12,27 @@ skips' _ 0  = []
 skips' xs n = skips' xs (subtract 1 n) ++ [accum]
   where accum = map (xs !!) (getNths n xs)
 {- Opted for readability (sorta) instead of pure code golfing -}
+
+
+-- Local Maxima
+localMaxima :: [Integer] -> [Integer]
+localMaxima xs = map getMax $ filter isMaxima $ map cleanCombo (combos xs)
+
+-- point-free version
+localMaxima' :: [Integer] -> [Integer]
+localMaxima' = map getMax . filter isMaxima . map cleanCombo . combos
+
+combos :: [Integer] -> [((Integer, Integer), Integer)]
+combos xs = zip (zip xs (tail xs)) (tail (tail xs))
+
+cleanCombo :: ((Integer, Integer), Integer) -> (Integer, Integer, Integer)
+cleanCombo ((a, b), c) = (a, b, c)
+
+cleanCombos :: [((Integer, Integer), Integer)] -> [(Integer, Integer, Integer)]
+cleanCombos = map cleanCombo
+
+isMaxima :: (Integer, Integer, Integer) -> Bool
+isMaxima (a,b,c) = (b > a) && (b > c)
+
+getMax :: (Integer, Integer, Integer) ->Integer
+getMax (_,b,_) = b
