25e940516dd246398e33a30728433d6dae71d369
Tony Poerio <tony@tonypoer.io>
2017-08-29 00:43:33 -0400

    added very concise (found) solution for histogram in ch3. worth reviewing

--- hw03/Golf.hs
+++ hw03/Golf.hs
@@ -1,4 +1,8 @@
 module Golf where
+{-# LANGUAGE FlexibleInstances #-}
+
+import Data.List
+
 -- Hopscotch
 skips :: [a] -> [[a]]
 skips xs = skips' xs (length xs)
@@ -36,3 +40,29 @@ isMaxima (a,b,c) = (b > a) && (b > c)
 
 getMax :: (Integer, Integer, Integer) ->Integer
 getMax (_,b,_) = b
+
+-- Histogram
+{- from: https://github.com/bschwb/cis194-solutions/blob/master/03-rec-poly/Golf.hs
+
+  Tried.. and read a few solutons, but this is the most concise I've seen.
+  I don't take cred it for it. But very nice algorithm.
+
+  What I initially had trouble with was mutating state. Here, we don't do that.
+  We create containing [(maxCount) ... 1], and for all frequencies,
+  if the the frequency at that number >= line number, then fill that line with '*'.
+
+  Also keps the data data structures to 2 lists, where the indices act as a implicit numbering system,
+  for both frequency counts, and line number.
+
+  Hadn't seen mapping a list comprehension over another list before, either. Nice touch.
+-}
+histogram :: [Integer] -> String
+histogram xs = unlines (map (line c) [m+1,m..1]) ++ "==========\n0123456789\n"
+  where c = count xs
+        m = maximum c
+
+line :: [Int] -> Int -> String
+line xs n = [if i >= n then '*' else ' ' | i <- xs]
+
+count :: [Integer] -> [Int]
+count xs = map (\n -> length $ filter (== n) xs) [0..9]
