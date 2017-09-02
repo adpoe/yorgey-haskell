3e87979c3ef913fa17ed4557836edf319169db2c
Tony Poerio <tony@tonypoer.io>
2017-09-01 23:08:33 -0400

    Finished tree fold

--- hw04/hw04.hs
+++ hw04/hw04.hs
@@ -2,7 +2,7 @@ module HW04
     (
     ) where
 
--- Wholemeal Programming
+--  | Wholemeal Programming
 {- re-write the `fun1` and `fun2` in more idiomatic Haskell style.
    break each into a pipeline of incremental transformations
    to an _entire_ data strucutre. -}
@@ -24,3 +24,24 @@ fun2 n
 
 fun2' :: Integer -> Integer
 fun2' = sum . filter even . takeWhile (/= 1) . iterate (\x -> if even x then (x `div` 2) else (3 * x + 1))
+
+-- | Binary Trees
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a) -- the integer tracks height
+  deriving (Show, Eq)
+
+-- foldr, because we are building from "bottom up"
+foldTree :: [a] -> Tree a
+foldTree xs = foldr insert Leaf xs
+
+insert :: a -> Tree a -> Tree a
+insert value Leaf = Node 0 Leaf value Leaf
+insert value (Node height left node right)
+  | leftHeight > rightHeight = Node (max (leftHeight+1) rightHeight) (insert value left) node right
+  | rightHeight >= leftHeight = Node (max leftHeight rightHeight+1) left node (insert value right)
+    where rightHeight = treeHeight left
+          leftHeight = treeHeight right
+
+treeHeight :: Tree a -> Integer
+treeHeight Leaf = 0
+treeHeight (Node height _ _ _) = height
