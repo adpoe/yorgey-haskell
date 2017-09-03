c31a509f8e5f6e2a06b98507c651cbfba9194d5d
Tony Poerio <tony@tonypoer.io>
2017-09-03 12:34:29 -0400

    Wk05, Ex02

--- hw05/Calculator.hs
+++ hw05/Calculator.hs
@@ -1,5 +1,7 @@
 module Calculator where
+
 import ExprT
+import Parser
 
 eval :: ExprT -> Integer
 eval (ExprT.Lit i) = i
@@ -9,3 +11,9 @@ eval (ExprT.Mul x y) = eval x * eval y
 -- e = Mul (Add (Lit 2) (Lit 3)) (Lit 4)
 -- eval e
 -- ---> 20
+evalStr :: String -> Maybe Integer
+evalStr s = maybeEval $ parseExp ExprT.Lit ExprT.Add ExprT.Mul s
+
+maybeEval :: Maybe ExprT -> Maybe Integer
+maybeEval (Just e) = Just (eval e)
+maybeEval Nothing = Nothing
