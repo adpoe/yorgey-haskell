a4e08481b4d9868ae86133b514bba5199d159b21
Tony Poerio <tony@tonypoer.io>
2017-09-05 00:03:12 -0400

    Ex4 of wk05

--- hw05/Calculator.hs
+++ hw05/Calculator.hs
@@ -17,3 +17,49 @@ evalStr s = maybeEval $ parseExp ExprT.Lit ExprT.Add ExprT.Mul s
 maybeEval :: Maybe ExprT -> Maybe Integer
 maybeEval (Just e) = Just (eval e)
 maybeEval Nothing = Nothing
+
+class Expr a where
+  lit :: Integer -> a
+  mul :: a -> a -> a
+  add :: a -> a -> a
+
+instance Expr ExprT where
+  lit = ExprT.Lit
+  add = ExprT.Add
+  mul = ExprT.Mul
+
+reify :: ExprT -> ExprT
+reify = id
+
+-- Additional Instances of the type classes
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit a = a > 0
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax a) (MinMax b) = MinMax (max a b)
+  mul (MinMax a) (MinMax b) = MinMax (min a b)
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7  where
+  lit a = Mod7 (mod a 7)
+  add (Mod7 a) (Mod7 b) = Mod7 (mod (a + b) 7)
+  mul (Mod7 a) (Mod7 b) = Mod7 (mod (a * b) 7)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
