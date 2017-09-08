b5e1cf4aaff4e5a4988e762f03ca335bbf65da0d
Tony Poerio <tony@tonypoer.io>
2017-09-08 00:00:42 -0400

    wk06, ex2

--- hw06/Fibonacci.hs
+++ hw06/Fibonacci.hs
@@ -4,8 +4,14 @@ module Fibonacci
 
 fib :: Integer -> Integer
 fib n
-  | n <= 1    = 1
+  | n <= 0    = 0
+  | n == 1    = 1
   | otherwise = fib (n-1) + fib (n-2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
+
+-- https://wiki.haskell.org/The_Fibonacci_sequence
+-- The "Canonical zipWith implementation"
+fibs2 :: [Integer]
+fibs2 = 0 : 1 : zipWith (+) fibs2 (tail fibs2)
